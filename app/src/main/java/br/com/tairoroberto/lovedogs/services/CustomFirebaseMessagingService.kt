7442a0acfa394b6ee0cb5238063a29f72790c448
tairoroberto <tairoroberto@hotmail.com>
2017-09-15 02:18:52 -0300

    Ajustando notificaçao

--- app/src/main/java/br/com/tairoroberto/lovedogs/services/CustomFirebaseMessagingService.kt
+++ app/src/main/java/br/com/tairoroberto/lovedogs/services/CustomFirebaseMessagingService.kt
@@ -4,14 +4,14 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.graphics.Color
 import android.media.RingtoneManager
+import android.net.Uri
 import android.support.v4.app.NotificationCompat
 import android.support.v4.content.ContextCompat
 import android.text.TextUtils
 import br.com.tairoroberto.lovedogs.R
-import br.com.tairoroberto.lovedogs.login.view.LoginActivity
 import br.com.tairoroberto.lovedogs.login.view.SplashActivity
+import br.com.tairoroberto.lovedogs.settings.ConfigDAO
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -38,18 +38,36 @@ class CustomFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun showNotification(title: String?, body: String?) {
         val intent = Intent(this, SplashActivity::class.java)
+        val config = ConfigDAO(this.applicationContext).getById(1)
+
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
         val builder = NotificationCompat.Builder(this)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setSmallIcon(R.drawable.ic_dog)
                 .setColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
+
+        if (config?.notification == true) {
+            if (!TextUtils.isEmpty(config.sound_notification)) {
+                val defaultSoundUri = Uri.parse(config.sound_notification)
+                builder.setSound(defaultSoundUri)
+            } else {
+                val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+                builder.setSound(defaultSoundUri)
+            }
+        }
+
+        val notification = builder.build()
+
+        if (config?.vibrate == true) {
+            notification.vibrate = longArrayOf(150, 300, 150, 600)
+        }
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, builder.build())
+        notificationManager.notify(0, notification)
     }
 }
